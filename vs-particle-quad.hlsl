b312030a1ff0a5fb4f2ca5aea180e945413d7ab2
cynic <daniel@szymanski.cc>
2019-11-13 21:29:05 +0100

    Added pixel shader for quad particle and first visualization

--- vs-particle-quad.hlsl
+++ vs-particle-quad.hlsl
@@ -21,23 +21,34 @@ struct Particle
 struct Output
 {
     float4 position : SV_POSITION;
-    float2 texCoord : TEXCOORD;
+    // float2 texCoord : TEXCOORD;
+    float4 color : COLOR;
 };
 
-StructuredBuffer<Particle> Particles : u0;
-StructuredBuffer<int> AliveParticles : u1;
+StructuredBuffer<Particle> Particles : t0;
+// StructuredBuffer<int> AliveParticles : u1;
 
-Output main(uint id: SV_VertexID)
+Output vsMain(uint id: SV_VertexID)
 {
     Output output;
 
     int quadIndex = id % 6;
     int particleId = id / 6;
     float3 quadPos = Quad[quadIndex];
-    Particle particle = Particles[AliveParticles[particleId]];
-    output.position = float4(particle.position + quadPos, 1.0);
+    // Particle particle = Particles[AliveParticles[particleId]];
+    Particle particle = Particles[particleId];
+    output.position = float4(particle.position*0.001 + quadPos*0.01/abs(particle.position.z), 1.0);
+    output.position.x *= 9.0/16.0;
+    output.color = particle.color;
+    // output.color.a = 1.0;
  //   output.texCoord = float2((id << 1) & 2, id & 2);
 
     return output;
 }
 
+
+float4 psMain(Output input) : SV_TARGET
+{
+    return input.color;
+    // return float4(1,0,1,1);
+}
