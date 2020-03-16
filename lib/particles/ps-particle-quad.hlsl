cfdc126b4d59206ba95e9862cfc53487cbb2edd0
pixtur <thomas.mann@framefield.com>
2020-03-16 23:26:06 +0100

    Some experiments with TypoGrid

--- lib/particles/ps-particle-quad.hlsl
+++ lib/particles/ps-particle-quad.hlsl
@@ -15,8 +15,9 @@ sampler texSampler : register(s0);
 
 float4 psMain(Output input) : SV_TARGET
 {
-    float f = inputTexture.Sample(texSampler, input.texCoord).r;
-    float c2= float4(1,1,1,1);
-    return c2;//input.color * f * c2;
+    float4 texColor = inputTexture.Sample(texSampler, input.texCoord);
+    //float c2= input.color;
+    return texColor * input.color;
+    //return c2;//input.color * f * c2;
     //return float4(1,1,1,1) * Color;
 }
--- proj-partial/fx/vs-typo-grid.hlsl
+++ proj-partial/fx/vs-typo-grid.hlsl
@@ -23,6 +23,12 @@ cbuffer Transforms : register(b0)
     float4x4 clipSpaceTobject;
 };
 
+cbuffer Params : register(b1)
+{
+    float4 Color;
+    float1 Size;
+};
+
 struct GridEntry
 {
     float3 position;
@@ -39,23 +45,33 @@ struct Output
 };
 
 StructuredBuffer<GridEntry> GridEntries : t0;
+Texture2D<float4> displaceTexture : register(t1);
+sampler texSampler : register(s0);
+
 
 Output vsMain(uint id: SV_VertexID)
 {
     Output output;
 
-    int quadIndex = id % 6;
+    int vertexIndex = id % 6;
     int entryIndex = id / 6;
-    float3 quadPos = Quad[quadIndex];
+    float3 quadPos = Quad[vertexIndex];
+
+
     GridEntry entry = GridEntries[entryIndex];
-    float4 worldPquadPos = mul(worldTobject, float4(entry.position,1));
-    worldPquadPos.xy += quadPos.xy*6.0 * entry.size;
+
+    float4 texColor = displaceTexture.SampleLevel(texSampler, entry.position.xy * 0.1, 0);
+    //float4 texColor = displaceTexture.SampleLevel(texSampler, float2(0,0) ,0 );
+    //texColor = float4(0.5,1,1,1);
+
+
+    float4 worldPquadPos = mul(worldTobject, float4(entry.position,1) + float4( texColor.b * -3, 0,0,0));
+    worldPquadPos.xy += quadPos.xy * entry.size * Size * texColor.g*4;
     float4 cameraPquadPos = mul(cameraTworld, worldPquadPos);
     output.position = mul(clipSpaceTcamera, cameraPquadPos);
     output.position.z = 0;
-    output.color = float4(0,1,1,1);
-    output.texCoord = entry.uv;
+    output.color = Color * texColor;
+    output.texCoord = (entry.uv + quadPos * float2(0.5, -0.5) + 0.5)/16;
 
     return output;
 }
-
