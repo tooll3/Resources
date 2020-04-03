ceb18815c278fc524a9fad64dbe579a9df9e97d4
pixtur <thomas.mann@framefield.com>
2020-04-03 18:54:35 +0200

    Fixed [bloom]-alpha rendering

--- lib/img/fx/image-fx-bloom.hlsl
+++ lib/img/fx/image-fx-bloom.hlsl
@@ -66,7 +66,7 @@ float4 psMain(vsOutput input) : SV_TARGET
 
     //c.rgb = float3(Offset, Offset, Offset) + c.rgb/totalWeight*Glow;
     //c.a = 1.0;
-    c = float4(Offset, Offset, Offset, Offset) + c/totalWeight*Glow2;
+    c = float4(Offset, Offset, Offset, 1) + c/totalWeight*Glow2;
     c.a = clamp(c.a, 0,1);
     //c.ra=1;
     return clamp(c,0,1000);
