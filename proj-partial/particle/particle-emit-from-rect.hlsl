18f2348acc717c9150cd6b530f10d0548e49e02f
Thomas Mann <thomas.mann@framefield.com>
2020-08-13 00:27:04 +0200

    Added renderGridShader

--- proj-partial/particle/particle-emit-from-rect.hlsl
+++ proj-partial/particle/particle-emit-from-rect.hlsl
@@ -27,15 +27,12 @@ cbuffer CountConstants : register(b2)
     int4 BufferCount;
 }
 
-// cbuffer Params : register(b3)
-// {
-//     float4 Color;
-//     float EmitPosY;
-//     float EmitPosYScatter;
-//     float Size;
-//     float Mass;
-//     float LifeTime;    
-// }
+cbuffer Params : register(b3)
+{
+    float LifeTime;
+    float LifeTimeScatter;
+    float EmitterId;
+}
 
 
 Texture2D<float4> inputTexture : register(t0);
@@ -62,6 +59,7 @@ void main(uint3 i : SV_DispatchThreadID)
     ParticleIndex pi = DeadParticles.Consume();
         
     Particle particle = Particles[pi.index];
+    particle.id = EmitterId;
     uint rng_state = uint(RunTime*1000.0)*10 + i.x;
 
     float u = float(wang_hash(rng_state)) * (1.0 / 4294967296.0);
@@ -73,7 +71,8 @@ void main(uint3 i : SV_DispatchThreadID)
     particle.velocity = float3(0,0,0);
     float u_mass = float(wang_hash(rng_state)) * (1.0 / 4294967296.0);
     particle.mass = 1.0 + step(0.05, u_mass)*999.0; // 5% with small mass
-    particle.lifetime = particle.mass > 5.0 ? 5.0 : 50.0;
+    //particle.lifetime = particle.mass > 5.0 ? 5.0 : 50.0;
+    particle.lifetime = LifeTime + u * LifeTimeScatter; 
 
     float s = 25.0;
     u = fmod(abs(particle.position.x), s)/s;
