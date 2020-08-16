cb89b2820a38443791b86e1aeaba77c760543be0
cynic <daniel@szymanski.cc>
2020-08-16 19:48:29 +0200

    Split lifetime update and updating the rest of the particle attributes

--- proj-partial/particle/particle-curl-noise-updater.hlsl
+++ proj-partial/particle/particle-curl-noise-updater.hlsl
@@ -22,6 +22,17 @@ cbuffer Transforms : register(b1)
     float4x4 ObjectToCamera;
     float4x4 ObjectToClipSpace;
 };
+
+cbuffer CountConstants : register(b2)
+{
+    int4 bufferCount;
+}
+
+cbuffer CountConstants : register(b3)
+{
+    float filterId;
+}
+
 // cbuffer Params : register(b1)
 // {
 //     float Frequency;
@@ -32,56 +43,28 @@ cbuffer Transforms : register(b1)
 
 RWStructuredBuffer<Particle> Particles : u0;
 RWStructuredBuffer<ParticleIndex> AliveParticles : u1;
-AppendStructuredBuffer<ParticleIndex> DeadParticles : u2;
-RWBuffer<uint> IndirectArgs : u3;
 
 [numthreads(64,1,1)]
 void main(uint3 i : SV_DispatchThreadID)
 {
-    // Initialize the draw args using the first thread in the Dispatch call
-    if (i.x == 0)
-    {
-        IndirectArgs[0] = 0;	// Number of primitives reset to zero
-        IndirectArgs[1] = 1;	// Number of instances is always 1
-        IndirectArgs[2] = 0;
-        IndirectArgs[3] = 0;
-    }
+    if (i.x >= bufferCount.x)
+        return; // only check alive particles
+    int index = AliveParticles[i.x].index;
+    Particle p = Particles[index];
+    if (filterId >= 0 && p.emitterId != filterId)
+        return; // not the relevant emitter id to process
 
-    // Wait after draw args are written so no other threads can write to them before they are initialized
-    GroupMemoryBarrierWithGroupSync();
+    // 2 lines below only relevant for sorting
+    // float3 posInCamera = mul(Particles[i.x].position, ObjectToCamera).xyz; // todo: optimize
+    // AliveParticles[index].squaredDistToCamera = posInCamera.z;//dot(-WorldToCamera[2].xyz, posInCamera);
 
-    float oldLifetime = Particles[i.x].lifetime;
-    float newLifetime = oldLifetime - (1.0/60.0);
+    float3 v = float3(0,0,0);
+    v += curlNoise(p.position*2.105)*0.1;
+    // v += curlNoise(Particles[i.x].position*0.0505);
+    // v += curlNoise(Particles[i.x].position*1.505);
+    p.velocity = v;//.xxx;
+    p.position += (1.0/60.)*(v.xxx);
 
-    if (newLifetime < 0.0)
-    {
-        if (oldLifetime >= 0.0)
-        {
-            ParticleIndex pi;
-            pi.index = i.x;
-            pi.squaredDistToCamera = 99999.0;
-            DeadParticles.Append(pi);
-        }
-    }
-    else
-    {
-        uint index = AliveParticles.IncrementCounter();
-        AliveParticles[index].index = i.x;
-        // 2 lines below only relevant for sorting
-        // float3 posInCamera = mul(Particles[i.x].position, ObjectToCamera).xyz; // todo: optimize
-        // AliveParticles[index].squaredDistToCamera = posInCamera.z;//dot(-WorldToCamera[2].xyz, posInCamera);
-
-        float3 v = float3(0,0,0);
-        v += curlNoise(Particles[i.x].position*0.105);
-        // v += curlNoise(Particles[i.x].position*0.0505);
-        // v += curlNoise(Particles[i.x].position*1.505);
-        Particles[i.x].velocity = v.xxx;
-        Particles[i.x].position += (1.0/60.)*(v.xxx);
-
-        uint originalValue;
-        InterlockedAdd(IndirectArgs[0], 6, originalValue);
-    }
-
-    Particles[i.x].lifetime = newLifetime;
+    Particles[index] = p;
 }
 
