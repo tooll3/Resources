336f1f2379bb33d8ec85563d5ee1ffef45707280
cynic <daniel@szymanski.cc>
2020-10-12 22:12:06 +0200

    Added first wannabe plane clipping code in slice-mesh shader

--- proj-partial/particle/slice-mesh.hlsl
+++ proj-partial/particle/slice-mesh.hlsl
@@ -10,7 +10,9 @@
 
 cbuffer EmitParameter : register(b0)
 {
-    float4 Plane;
+    float3 Plane;
+    float dummy;
+    float3 PlanePos;
 };
 
 cbuffer TimeConstants : register(b1)
@@ -60,26 +62,103 @@ void main(uint3 i : SV_DispatchThreadID)
         return; 
 
     uint index = i.x;
-
     Face f = PointCloud[index];
 
-    // if (i.x % 2 == 1)
-    //     return;
-    if (f.positions[0].z < Plane.w && f.positions[1].z < Plane.w && f.positions[2].z < Plane.w)
-        return;
+    float np[3];
+    np[0] = dot(Plane, f.positions[0] - PlanePos);
+    np[1] = dot(Plane, f.positions[1] - PlanePos);
+    np[2] = dot(Plane, f.positions[2] - PlanePos);
+    if (np[0] < 0 && np[1] < 0 && np[2] < 0)
+        return; // all points skipped
 
-    for (uint j = 0; j < 3; j++)
+    if (np[0] > 0 && np[1] > 0 && np[2] > 0)
     {
-        if (f.positions[j].z < Plane.w)
-            f.positions[j].z = Plane.w;
+        uint targetIndex = SlicedData.IncrementCounter();
+        SlicedData[targetIndex] = f;
+        return; // all points on 'right' side of the plane, just keep input triangle
     }
 
+    if (np[0] > 0 && np[1] < 0 && np[2] < 0)
+    {
+        // one point 'in' two 'out', -> one tri
+        float3 lDir = (f.positions[1] - f.positions[0]);
+        float d = np[1] / dot(lDir, Plane);
+        d = dot(PlanePos - f.positions[0], Plane) / dot(lDir, Plane);
+        f.positions[1] = f.positions[0] + lDir*d;
 
-    // float dist = dot(f.positions[0] - float3(0,0,Plane.w), Plane.xyz);
-    // if (dist < 0)
-        // return;
+        lDir = f.positions[2] - f.positions[0];
+        d = dot(PlanePos - f.positions[0], Plane) / dot(lDir, Plane);
+        f.positions[2] = f.positions[0] + lDir*d;;
 
-    uint targetIndex = SlicedData.IncrementCounter();
-    SlicedData[targetIndex] = f;
+        uint targetIndex = SlicedData.IncrementCounter();
+        SlicedData[targetIndex] = f;
+        return;
+    }
+
+    if (np[1] > 0 && np[0] < 0 && np[2] < 0)
+    {
+        // one point 'in' two 'out', -> one tri
+        float3 lDir = (f.positions[0] - f.positions[1]);
+        float d = np[0] / dot(lDir, Plane);
+        d = dot(PlanePos - f.positions[1], Plane) / dot(lDir, Plane);
+        f.positions[0] = f.positions[1] + lDir*d;
+
+        lDir = f.positions[2] - f.positions[1];
+        d = dot(PlanePos - f.positions[1], Plane) / dot(lDir, Plane);
+        f.positions[2] = f.positions[1] + lDir*d;;
+
+        uint targetIndex = SlicedData.IncrementCounter();
+        SlicedData[targetIndex] = f;
+        return;
+    }
+
+    if (np[2] > 0 && np[0] < 0 && np[1] < 0)
+    {
+        // one point 'in' two 'out', -> one tri
+        float3 lDir = (f.positions[0] - f.positions[2]);
+        float d = dot(PlanePos - f.positions[2], Plane) / dot(lDir, Plane);
+        f.positions[0] = f.positions[2] + lDir*d;
+
+        lDir = f.positions[1] - f.positions[2];
+        d = dot(PlanePos - f.positions[2], Plane) / dot(lDir, Plane);
+        f.positions[1] = f.positions[2] + lDir*d;;
+
+        uint targetIndex = SlicedData.IncrementCounter();
+        SlicedData[targetIndex] = f;
+        return;
+    }
+
+    for (int ind = 0; ind < 3; ind++)
+    {
+        int i0 = ind;
+        int i1 = (i0 + 1) % 3;
+        int i2 = (i0 + 2) % 3;
+        if (np[i0] < 0 && np[i1] > 0 && np[i2] > 0)
+        {
+            Face f1 = f;
+            float3 lDir = (f.positions[i2] - f.positions[i0]);
+            // float d = np[i2] / dot(lDir, Plane);
+            float d = dot(PlanePos - f.positions[i0], Plane) / dot(lDir, Plane);
+            float3 newP0To2 = f.positions[i0] + lDir*d;
+            f1.positions[i0] = newP0To2;
+
+            Face f2 = f;
+            lDir = f.positions[i1] - f.positions[i0];
+            // d = np[i1] / dot(lDir, Plane);
+            d = dot(PlanePos - f.positions[i0], Plane) / dot(lDir, Plane);
+            float3 newP0To1 = f.positions[i0] + lDir*d;
+            f2.positions[i0] = newP0To1;
+            f2.positions[i2] = newP0To2;
+
+            uint targetIndex = SlicedData.IncrementCounter();
+            SlicedData[targetIndex] = f1;
+            targetIndex = SlicedData.IncrementCounter();
+            SlicedData[targetIndex] = f2;
+            return;
+        }
+    }
+
+    // uint targetIndex = SlicedData.IncrementCounter();
+    // SlicedData[targetIndex] = f;
 }
 
