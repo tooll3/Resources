b19a4bb4b608ff30aa1e63eb4763f0a24e186000
Thomas Mann <thomas.mann@framefield.com>
2020-10-13 00:35:26 +0200

    Add SamplePoint Ops

---
+++ lib/points/sample-point-attributes.hlsl
@@ -0,0 +1,84 @@
+
+static const float4 Factors[] = 
+{
+  //     x  y  z  w
+  float4(0, 0, 0, 0), 
+  float4(1, 0, 0, 0),
+  float4(0, 1, 0, 0),
+  float4(0, 0, 1, 0),
+  float4(0, 0, 0, 1),
+};
+
+cbuffer Transforms : register(b0)
+{
+    float4x4 CameraToClipSpace;
+    float4x4 ClipSpaceToCamera;
+    float4x4 WorldToCamera;
+    float4x4 CameraToWorld;
+    float4x4 WorldToClipSpace;
+    float4x4 ClipSpaceToWorld;
+    float4x4 ObjectToWorld;
+    float4x4 WorldToObject;
+    float4x4 ObjectToCamera;
+    float4x4 ObjectToClipSpace;
+};
+
+cbuffer Params : register(b1)
+{
+    float L;
+    float LFactor;
+    float LOffset;
+
+    float R;
+    float RFactor;
+    float ROffset;
+
+    float G;
+    float GFactor;
+    float GOffset;
+
+    float B;
+    float BFactor;
+    float BOffset;
+
+    // float A;
+    // float AFactor;
+    // float AOffset;
+    float __padding;
+
+    float3 Center;
+}
+
+struct Point {
+    float3 Position;
+    float W;
+};
+
+StructuredBuffer<Point> Points : t0;
+RWStructuredBuffer<Point> ResultPoints : u0;    // output
+
+
+Texture2D<float4> inputTexture : register(t1);
+sampler texSampler : register(s0);
+
+[numthreads(256,4,1)]
+void main(uint3 i : SV_DispatchThreadID)
+{
+    uint index = i.x; 
+
+    Point P = Points[index];
+    float3 pos = P.Position;
+    pos -= Center;
+    
+    float3 posInObject = mul(float4(pos.xyz,0), WorldToObject).xyz;
+    float4 c = inputTexture.SampleLevel(texSampler, posInObject.xy + 0.5 , 0.0);
+
+    uint RMode = (uint)clamp(R, 0, 4);
+    float4 rF = Factors[(uint)clamp(R, 0, 4.1)] * c * RFactor + ROffset; 
+    float4 gF = Factors[(uint)clamp(G, 0, 4.1)] * c * GFactor + GOffset; 
+    float4 bF = Factors[(uint)clamp(B, 0, 4.1)] * c * BFactor + BOffset; 
+    float4 f = rF+gF+bF;
+
+    ResultPoints[index].Position = P.Position + float3(f.xyz);
+    ResultPoints[index].W = P.W + f.w;
+}
