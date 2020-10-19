7b3bc47f2c958195e1328b20f50153d93c9b2ff8
Thomas Mann <thomas.mann@framefield.com>
2020-10-19 19:34:24 +0200

    Updated triangle transition

--- lib/img/fx/img-fx-TriangleGridTransition.hlsl
+++ lib/img/fx/img-fx-TriangleGridTransition.hlsl
@@ -8,7 +8,8 @@ cbuffer ParamConstants : register(b0)
     float MixOriginal;
     float Rotation;
     float EffectWidth;
-    float TestParam;
+    float Scatter;
+    float Gap;
     float2 EffectCenter;
 }
 
@@ -33,7 +34,7 @@ struct vsOutput
 };
 
 Texture2D<float4> ImageA : register(t0);
-Texture2D<float> Effects : register(t1);
+Texture2D<float4> ImageB : register(t1);
 sampler texSampler : register(s0);
 
 #define mod(x, y) ((x) - (y) * floor((x) / (y)))
@@ -69,7 +70,6 @@ float4 psMain(vsOutput psInput) : SV_TARGET
     // Effect ramp
     float radians = Rotation / 180 *3.141578;
     float2 angle =  float2(sin(radians),cos(radians));
-    float4 orgColor = ImageA.Sample(texSampler, psInput.texCoord);
     float fx=  dot(p-EffectCenter, angle) / EffectWidth / 2 + 0.5;
 
 
@@ -87,34 +87,39 @@ float4 psMain(vsOutput psInput) : SV_TARGET
     float2 line2 = mul(bottomCenter, rot(-1.107));
     float isPointingDown = line1.y >= 0 && line2.y >= 0 ? 0 :1;
     float isRightSide = bottomCenter.x > 0 ? 1 :0;
-    float2 line3 = lerp(topCenter.y, bottomCenter.y, isPointingDown);
-
-    topCenter +=  float2(isPointingDown * isRightSide * (halfStep.x - 3.1417/4 ) * Divisions,0);
-
-    float2 center = isPointingDown 
-            ? topCenter + float2(0,  0.69)
-            : bottomCenter - float2(0,  0.69);
-
-    float2 cellId = ((p+oddRowOffsetOffset - posInCell) * Divisions) * float2(2,1);
-    cellId -= float2(isPointingDown * (isRightSide ? -1 : 1),0);
+    float2 line3 = lerp(topCenter, bottomCenter, isPointingDown);
 
     float2 posInTriange = posInCell + isPointingDown * 
              (float2(isPointingDown * (isRightSide ? -1 : 1) * halfStep.x  ,0)
              );
 
+    float2 cellId = p-posInTriange;
+
     float distanceFromEdge = min( abs(line1.y), min(abs(line2.y), abs(line3.y)));
 
     float2 hash = hash22(cellId+2000);
-    float cellFx=  dot(p-EffectCenter - posInTriange, angle) / EffectWidth / 2 + 0.5 + TestParam * (hash.y - 0.5);
-    float celShade = sin((beatTime + hash * 999) ) +1;
-    float lineThickness = LineThickness* 1*cellFx;
-    float edge = smoothstep(lineThickness+0.01, lineThickness + 0.015, distanceFromEdge);
+
+    float blendFx=  dot(p-EffectCenter - posInTriange, angle) / EffectWidth   + Scatter * (hash.y - 0.5);
+
+    if(abs(blendFx) < Gap) {
+        return Background;
+    }
+
+    float4 orgColor = blendFx < 0 
+        ? ImageA.Sample(texSampler, psInput.texCoord)
+        : ImageB.Sample(texSampler, psInput.texCoord);
 
     float2 uv = psInput.texCoord;
-    //float4 orgColor = ImageA.Sample(texSampler, uv );
+    float cellFx = 1-saturate( abs(blendFx)-Gap);
 
-    //return float4(c,0,0,1);
+    float celShade = sin((beatTime + hash * 999) ) +1;
+    float lineThickness = -0.02+LineThickness* cellFx;
+    float edge = smoothstep(lineThickness+0.01, lineThickness + 0.02, distanceFromEdge);
+    lineThickness += 0.02;
+    float highlightEdge= smoothstep(lineThickness+0.01, lineThickness + 0.02, distanceFromEdge) * cellFx;
+    //return float4(highlightEdge -edge,0,0,1);
+    orgColor = lerp(orgColor, Fill, highlightEdge); 
+
     float fxShade = lerp(1, saturate(celShade-cellFx), saturate(cellFx));
-
-    return float4((orgColor.rgb * fxShade), (1-saturate(cellFx)) *edge);
+    return lerp(Background, orgColor,(1-saturate(cellFx)) *edge);
 }
