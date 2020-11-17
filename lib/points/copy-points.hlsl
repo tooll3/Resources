e48f0efc5741bd32ca96b20f94e81c060b514a79
Thomas Mann <thomas.mann@framefield.com>
2020-11-17 21:57:45 +0100

    Update CopyPoints and PointsFromMeshData

--- lib/points/copy-points.hlsl
+++ lib/points/copy-points.hlsl
@@ -4,8 +4,18 @@
 StructuredBuffer<Point> PointsB : t0;         // input
 RWStructuredBuffer<Point> ResultPoints : u0; 
 
+
+
+cbuffer Params : register(b0)
+{
+    float MixOriginal;
+};
+
+
 [numthreads(64,1,1)]
 void main(uint3 i : SV_DispatchThreadID)
 {
-    ResultPoints[i.x] = PointsB[i.x];
+    ResultPoints[i.x].position = lerp(ResultPoints[i.x].position,  PointsB[i.x].position, MixOriginal);
+    ResultPoints[i.x].w = lerp(ResultPoints[i.x].w,  PointsB[i.x].w, MixOriginal);
+    ResultPoints[i.x].rotation = normalize(lerp(ResultPoints[i.x].rotation,  PointsB[i.x].rotation, MixOriginal));
 }
