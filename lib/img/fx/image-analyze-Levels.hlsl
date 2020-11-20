ea6b9648b6f3e4e7b4e39aa42802da1cb1f4c1fc
Thomas Mann <thomas.mann@framefield.com>
2020-11-20 01:06:24 +0100

    Improve analyze image operator

--- lib/img/fx/image-analyze-Levels.hlsl
+++ lib/img/fx/image-analyze-Levels.hlsl
@@ -1,14 +1,8 @@
 cbuffer ParamConstants : register(b0)
 {
-    //float4 Fill;
-    //float4 Background;
     float2 Center;
     float Width;
     float Rotation;
-    float TestParam;
-    // float PingPong;
-    // float Repeat;
-    // float Bias;
 }
 
 cbuffer Resolution : register(b2)
@@ -39,84 +33,35 @@ float4 psMain(vsOutput psInput) : SV_TARGET
     inputTexture.GetDimensions(width, height);
 
     float2 uv = psInput.texCoord;
-    //float4 orgColor = inputTexture.SampleLevel(texSampler, uv, 0.0);
 
     float aspectRation = TargetWidth/TargetHeight;
     float2 p = uv;
     p-= 0.5;
     p.x *=aspectRation;
 
-    //float2 p = psInput.texCoord;
-
     float radians = Rotation / 180 *3.141578;
     float2 angle =  float2(sin(radians),cos(radians));
-
-
     float distanceFromCenter=  dot(p-Center, angle);
     float normalizedDistance  = -distanceFromCenter / Width;
     if(normalizedDistance < 0) {
-        //return float4(p* 10 %1,0,1);
         return inputTexture.Sample(texSampler, uv);
     }
 
     if( IsBetween(normalizedDistance, 1, 1.01)) {
-        return float4(0.4,0.4,0.4,1);
+        return float4(0.2, 0.2, 0.2, 1);
     }
 
-    // if( IsBetween(normalizedDistance, 0, 0.01)) {
-    //     return float4(0,0,0,1);
-    // }
+    float2 pOnLine = p;
+    pOnLine +=  (- distanceFromCenter)  *  angle;
+    pOnLine.x /= aspectRation;
+    pOnLine += 0.5;
+    float4 colorOnLine = inputTexture.Sample(texSampler, pOnLine);
+    float4 curveColor = float4(0,0,0,1);
 
+    curveColor.rgb = (colorOnLine.rgb < normalizedDistance ) ? 0:0.3;
+    curveColor.rgb *= (colorOnLine.rgb < normalizedDistance + 0.02 ) ? 3:1;
 
-    //if(normalizedDistance < 1)  {
-        //p.x *= aspectRation;
-        //p.y+= 0.25;        
-        float2 pOnLine = p;
-        pOnLine +=  (- distanceFromCenter)  *  angle;
-        pOnLine.x /= aspectRation;
-        pOnLine += 0.5;
-        //return float4(pOnLine * 10 %1,0,1);
-        //float2 pOnLine = p-Center +  distanceFromCenter * angle;
-        float4 colorOnLine = inputTexture.Sample(texSampler, pOnLine);
-
-        float4 curveColor = float4(0,0,0,1);
-
-        curveColor.rgb = (colorOnLine.rgb < normalizedDistance ) ? 0:0.2;
-        curveColor.rgb *= (colorOnLine.rgb < normalizedDistance + 0.01 ) ? 3:1;
-        //curveColor.r = (colorOnLine.r < normalizedDistance ) ? 1:0;
-
-        return curveColor;
-    //}
-
-    //return float4(0,0,0,1);
     
-    //float4 orgColor = 
-
-
-    // c = PingPong > 0.5 
-    //     ? (Repeat < 0.5 ? (abs(c) / Width)
-    //                     : 1-abs( fmod(c,Width *2) -Width)  / Width)
-    //     : c / Width + 0.5;
-
-    // c = Repeat > 0.5 
-    //     ? fmod(c,1)
-    //     : saturate(c);
-
-    // if(Smooth > 0.5) {
-    //     c= smoothstep(0,1,c);
-    // }
-
-    // float dBiased = Bias>= 0 
-    //     ? pow( c, Bias+1)
-    //     : 1-pow( clamp(1-c,0,10), -Bias+1);
-
-    //float4 cOut= lerp(Fill, Background, dBiased);
-
-    //float4 rgb = float39
-
-    // float4 gradient = Gradient.Sample(texSampler, float2(dBiased, 0));
-    // float a = orgColor.a + gradient.a - orgColor.a*gradient.a;
-    // float3 rgb = (1.0 - gradient.a)*orgColor.rgb + gradient.a*gradient.rgb;   
-
-    //return float4(rgb,a);
+    curveColor.rgb += inputTexture.Sample(texSampler, uv) * ((normalizedDistance <1) ? 0.6 : 1);
+    return curveColor;
 }
--- lib/img/fx/img-fx-SortPixelsGlitch.hlsl
+++ lib/img/fx/img-fx-SortPixelsGlitch.hlsl
@@ -5,6 +5,7 @@ cbuffer ParamConstants : register(b0)
 {
     float Range;
     float Threshold;
+    float TestParam;
 }
 
 
@@ -34,9 +35,7 @@ struct vsOutput
 float4 psMain(vsOutput psInput) : SV_TARGET
 {
     float2 uv = psInput.texCoord;
-
     int steps = (int)clamp(Range,1,600);
-
     float2 direction = 1/ float2(TargetWidth, TargetHeight);
     direction.y=0;
 
@@ -70,6 +69,8 @@ float4 psMain(vsOutput psInput) : SV_TARGET
     return minColor;
 */
 
+
+/*
     float maxColorStep=0;
     float minColorStep=0;
     float maxColorSum = -1;
@@ -77,7 +78,6 @@ float4 psMain(vsOutput psInput) : SV_TARGET
     float4 minColor = float4(0,0,0,1);
     float4 maxColor =1;
     
-    //float4 c;
     float sum = 0;
     float4 orgC = inputTexture.SampleLevel(texSampler, uv, 0.0);
     float orgSum = orgC.r + orgC.g + orgC.b;
@@ -89,8 +89,8 @@ float4 psMain(vsOutput psInput) : SV_TARGET
     {
         c = inputTexture.SampleLevel(texSampler, uv - direction * leftSteps, 0.0);
         sum = c.r + c.g + c.b;
-        if(abs(sum -orgSum)  > Threshold)  {
-        //if(sum > orgSum  + Threshold)  {
+        //if(abs(sum -orgSum)  > Threshold)  {
+        if(sum > orgSum  + Threshold)  {
             
             leftColor=c;
             break;
@@ -109,8 +109,8 @@ float4 psMain(vsOutput psInput) : SV_TARGET
     {
         c = inputTexture.SampleLevel(texSampler, uv + direction * rightSteps, 0.0);
         sum = c.r + c.g + c.b;
-        if( abs(sum - orgSum) >  Threshold)  
-        //if( sum  <  orgSum - Threshold)  
+        //if( abs(sum - orgSum) >  Threshold)  
+        if( sum  <  orgSum - Threshold)  
         {
             
             c=rightColor;            
@@ -124,18 +124,16 @@ float4 psMain(vsOutput psInput) : SV_TARGET
         rightColor = sumColorRight;
     }
     
-    //return maxColor;
     float f = leftSteps*1./(leftSteps + rightSteps);
     float center = (leftSteps + rightSteps * 1.)/2.;
     float distanceFromCenter = abs(center-f)/5.;
-    //return float4(distanceFromCenter/30.,0,0,1);
-    // return float4(
-    //     leftSteps/100. * 0, 
-    //     rightSteps/100. * 0, 
-    //     f * 1,
-    //     1);
+    return float4(distanceFromCenter/100.,0,0,1);
+    return float4(
+        leftSteps/100. * 0, 
+        rightSteps/100. * 0, 
+        f * 1,
+        1);
 
-    //return float4(f,0,0,1);
     if(f < 0.5) {
         return lerp( rightColor, orgC, f*2);    
     }
@@ -144,17 +142,124 @@ float4 psMain(vsOutput psInput) : SV_TARGET
     }
     
 
-    //maxColor= max(maxColor,inputTexture.SampleLevel(texSampler, uv,0) );
+    float test = minColorStep/Range;
+    */
+
+
+    float maxColorStep=0;
+    float minColorStep=0;
+    float maxColorSum = -1;
+    float minColorSum = 999;
+    float4 minColor = float4(0,0,0,1);
+    float4 maxColor =1;
+    
+    float sum = 0;
+    float4 orgC = inputTexture.SampleLevel(texSampler, uv, 0.0);
+    float orgSum = orgC.r + orgC.g + orgC.b;
+
+    float4 sumColorLeft = 0;
+    float4 leftColor=orgC;
+
+    //float FallOff = 0.01;
+
+    // Scan left to find outer peak
+    float4 c;
+    float leftMaxSum = 0;
+    float leftMaxStep =0;
+    float4 leftMaxColor;
+
+    for(int leftSteps = 1; leftSteps< steps; leftSteps++) 
+    {
+        c = inputTexture.SampleLevel(texSampler, uv - direction * leftSteps, 0.0);
+        sum = (c.r + c.g + c.b);
+        //if(abs(sum -orgSum)  > Threshold)  {
+        if(sum >= leftMaxSum +  TestParam)  
+        {
+            leftMaxStep = leftSteps;
+            leftMaxSum = sum;
+            leftMaxColor = c;            
+        }
+        sumColorLeft += c;
+    }
+    //return float4(leftMaxStep/Range*0.8, 0,0,1);
+
+    
+    float rightMaxSum = 10;
+    float rightMaxStep =0;
+    float4 rightMaxColor;
+    float4 sumColorRight;
+
+    for(int rightSteps = 1; rightSteps< steps; rightSteps++) 
+    {
+        c = inputTexture.SampleLevel(texSampler, uv + direction * rightSteps, 0.0);
+        sum = (c.r + c.g + c.b);
+        //if(abs(sum -orgSum)  > Threshold)  {
+        if(sum <= rightMaxSum - rightSteps * 0.01 )  
+        {
+            rightMaxStep = rightSteps;
+            rightMaxSum = sum;
+            rightMaxColor = c;            
+        }
+        sumColorRight += c;
+    }
+    return float4(
+        leftMaxStep/Range * 1,     
+        rightMaxStep/Range * 1, 
+        ((leftMaxStep/Range - rightMaxStep/Range) + 0.25) *0,
+    1);
+
+/*
+    float4 sumColorRight = 0;
+    float4 rightColor=orgC;
+    for(int rightSteps = 1; rightSteps< steps; rightSteps++) 
+    {
+        c = inputTexture.SampleLevel(texSampler, uv + direction * rightSteps, 0.0);
+        sum = c.r + c.g + c.b;
+        //if( abs(sum - orgSum) >  Threshold)  
+        if( sum  <  orgSum - Threshold)  
+        {
+            
+            c=rightColor;            
+            break;
+        }
+        sumColorRight += rightColor;
+    }
+    if(rightSteps == steps) 
+    {
+        sumColorRight /= rightSteps;
+        rightColor = sumColorRight;
+    }
+    
+    float f = leftSteps*1./(leftSteps + rightSteps);
+    float center = (leftSteps + rightSteps * 1.)/2.;
+    float distanceFromCenter = abs(center-f)/5.;
+    return float4(distanceFromCenter/100.,0,0,1);
+    return float4(
+        leftSteps/100. * 0, 
+        rightSteps/100. * 0, 
+        f * 1,
+        1);
+
+    if(f < 0.5) {
+        return lerp( rightColor, orgC, f*2);    
+    }
+    else {
+        return lerp( orgC, leftColor,  f*2-1);    
+    }
+    
 
     float test = minColorStep/Range;
-    
-    //return float4(0, 0 ,test2 ,1);
+*/
+
+    return float4(0,0,0,1);
 
 
-    //return c;
 
 
-    // float a2 = clamp(Background.a + c.a - Background.a*c.a, 0,1);
-    // float3 rgb2 = clamp((1.0 - c.a)*Background.rgb + c.a*c.rgb, 0.00001, 100);   
-    // return float4(rgb2, a2);
+
+
+
+
+
+
 }
