b58d1c5bd86250ec88610155b223ea52d2bb44b8
Thomas Mann <thomas.mann@framefield.com>
2021-01-24 21:16:15 +0100

    Add RenderMesh and removed some commented code

--- lib/points/render-mesh.hlsl
+++ lib/points/render-mesh.hlsl
@@ -2,16 +2,6 @@
 #include "point-light.hlsl"
 #include "pbr.hlsl"
 
-struct PbrVertex
-{
-    float3 Position;
-    float3 Normal;
-    float3 Tangent;
-    float3 Bitangent;
-    float2 TexCoord;
-    float2 __padding;
-};
-
 cbuffer Transforms : register(b0)
 {
     float4x4 CameraToClipSpace;
@@ -77,40 +67,23 @@ sampler texSampler : register(s0);
 
 StructuredBuffer<PbrVertex> PbrVertices : t0;
 StructuredBuffer<int3> FaceIndices : t1;
-StructuredBuffer<Point> Points : t2;
 
-Texture2D<float4> BaseColorMap : register(t3);
-Texture2D<float4> EmissiveColorMap : register(t4);
-Texture2D<float4> RSMOMap : register(t5);
-Texture2D<float4> NormalMap : register(t6);
+Texture2D<float4> BaseColorMap : register(t2);
+Texture2D<float4> EmissiveColorMap : register(t3);
+Texture2D<float4> RSMOMap : register(t4);
+Texture2D<float4> NormalMap : register(t5);
 
 
 psInput vsMain(uint id: SV_VertexID)
 {
     psInput output;
 
-    uint faceCount, meshStride;
-    FaceIndices.GetDimensions( faceCount,meshStride);
-
-    int verticesPerInstance = faceCount * 3;
-
-    int faceIndex = (id % verticesPerInstance) / 3;
+    int faceIndex = id / 3;//  (id % verticesPerInstance) / 3;
     int faceVertexIndex = id % 3;
 
-    uint instanceCount, instanceStride;
-    Points.GetDimensions( instanceCount,instanceStride);
-
-    int instanceIndex = id / verticesPerInstance;
-
     PbrVertex vertex = PbrVertices[FaceIndices[faceIndex][faceVertexIndex]];
+
     float4 posInObject = float4( vertex.Position,1);
-
-    float4x4 orientationMatrix = transpose(quaternion_to_matrix(Points[instanceIndex].rotation));
-
-    posInObject.xyz *= Points[instanceIndex].w * Size;
-    posInObject = mul( float4(posInObject.xyz, 1), orientationMatrix) ;
-
-    posInObject += float4(Points[instanceIndex].position, 0); 
 
     float4 posInClipSpace = mul(posInObject, ObjectToClipSpace);
     output.pixelPosition = posInClipSpace;
@@ -120,7 +93,6 @@ psInput vsMain(uint id: SV_VertexID)
 
     // Pass tangent space basis vectors (for normal mapping).
     float3x3 TBN = float3x3(vertex.Tangent, vertex.Bitangent, vertex.Normal);
-    TBN = mul(TBN, (float3x3)orientationMatrix);
     TBN = mul(TBN, (float3x3)ObjectToWorld);
     output.tbnToWorld = TBN;
 
