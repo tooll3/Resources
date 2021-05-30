2666414b8b3483080d5d40623fc1ac8b6c508fc1
Thomas Mann <thomas.mann@framefield.com>
2021-05-30 21:41:44 +0200

    Add TextureToCubeMap

--- lib/3d/rendering/equirect-texture-to-cubemap.hlsl
+++ lib/3d/rendering/equirect-texture-to-cubemap.hlsl
@@ -1,52 +1,84 @@
 #include "point.hlsl"
 
 
-static const float3 Quad[] = 
-{
-  // xy front
-  float3(-1, -1, 1),
-  float3( 1, -1, 1), 
-  float3( 1,  1, 1), 
-  float3( 1,  1, 1), 
-  float3(-1,  1, 1), 
-  float3(-1, -1, 1), 
-  // yz right
-  float3(1, -1,  1),
-  float3(1, -1, -1),
-  float3(1,  1, -1),
-  float3(1,  1, -1),
-  float3(1,  1,  1), 
-  float3(1, -1,  1),
-  // xz top
-  float3(-1, 1,  1),
-  float3( 1, 1,  1),
-  float3( 1, 1, -1),
-  float3( 1, 1, -1),
-  float3(-1, 1, -1),
-  float3(-1, 1,  1),
-  // xy back
-  float3( 1, -1, -1),
-  float3(-1, -1, -1),
-  float3(-1,  1, -1),
-  float3(-1,  1, -1),
-  float3( 1,  1, -1),
-  float3( 1, -1, -1),
-  // yz left
-  float3(-1, -1, -1),
-  float3(-1, -1,  1),
-  float3(-1,  1,  1),
-  float3(-1,  1,  1),
-  float3(-1,  1, -1),
-  float3(-1, -1, -1),
-  // xz bottom
-  float3(-1, -1,  1),
-  float3( 1, -1,  1),
-  float3( 1, -1, -1),
-  float3( 1, -1, -1),
-  float3(-1, -1, -1),
-  float3(-1, -1,  1),
-};
+// static const float3 Quad[] = 
+// {
+//   // xy front
+//   float3(-1, -1, 1),
+//   float3( 1, -1, 1), 
+//   float3( 1,  1, 1), 
+//   float3( 1,  1, 1), 
+//   float3(-1,  1, 1), 
+//   float3(-1, -1, 1), 
+//   // yz right
+//   float3(1, -1,  1),
+//   float3(1, -1, -1),
+//   float3(1,  1, -1),
+//   float3(1,  1, -1),
+//   float3(1,  1,  1), 
+//   float3(1, -1,  1),
+//   // xz top
+//   float3(-1, 1,  1),
+//   float3( 1, 1,  1),
+//   float3( 1, 1, -1),
+//   float3( 1, 1, -1),
+//   float3(-1, 1, -1),
+//   float3(-1, 1,  1),
+//   // xy back
+//   float3( 1, -1, -1),
+//   float3(-1, -1, -1),
+//   float3(-1,  1, -1),
+//   float3(-1,  1, -1),
+//   float3( 1,  1, -1),
+//   float3( 1, -1, -1),
+//   // yz left
+//   float3(-1, -1, -1),
+//   float3(-1, -1,  1),
+//   float3(-1,  1,  1),
+//   float3(-1,  1,  1),
+//   float3(-1,  1, -1),
+//   float3(-1, -1, -1),
+//   // xz bottom
+//   float3(-1, -1,  1),
+//   float3( 1, -1,  1),
+//   float3( 1, -1, -1),
+//   float3( 1, -1, -1),
+//   float3(-1, -1, -1),
+//   float3(-1, -1,  1),
+// };
 
+
+float4 colorOfBox(uint face)
+{
+    float4 c = float4(0,0,0,1);
+
+    if (face == 0) // posx (red)
+    {
+        c = float4(1,0,0,1);
+    }
+    else if (face == 1) // negx (cyan)
+    {
+        c = float4(1,1,0,1);
+    }
+    else if (face == 2) // posy (green)
+    {
+        c = float4(0,1,0,1);
+    }
+    else if (face == 3) // negy (magenta)
+    {
+        c = float4(0,1,1,1);
+    }
+    else if (face == 4) // posz (blue)
+    {
+        c = float4(0,0,1,1);
+    }
+    else // if (i.face == 5) // negz (yellow)
+    {
+        c = float4(1,0,1,1);
+    }
+ 
+    return c;
+}
 
 
 float3 UvAndIndexToBoxCoord(float2 uv, uint face)
@@ -54,42 +86,37 @@ float3 UvAndIndexToBoxCoord(float2 uv, uint face)
     float3 n = float3(0,0,0);
     float3 t = float3(0,0,0);
 
-    // xy front
-    if (face == 0) // negz (yellow)
-    {
-        n = float3(0,0,1);
-        t = float3(0,1,0);
-    }
-        // yz right    
-    else if (face == 1) // posx (red)
+    if (face == 0) // posx (red)
     {
         n = float3(1,0,0);
         t = float3(0,1,0);
     }
-        // xz top
-    else if (face == 2) // negy (magenta)
-    {
-        n = float3(0,1,0);
-        t = float3(0,0,1);
-    }
-        // xy back
-    else if (face == 3) // posz (blue)
-    {
-        n = float3(0,0,-1);
-        t = float3(0,1,0);
-    }
-        // yz left
-    else if (face == 4) // negx (cyan)
+    else if (face == 1) // negx (cyan)
     {
         n = float3(-1,0,0);
         t = float3(0,1,0);
     }
-    // xz bottom
-    else if (face == 5) // posy (green)
+    else if (face == 2) // posy (green)
     {
         n = float3(0,-1,0);
         t = float3(0,0,-1);
     }
+    else if (face == 3) // negy (magenta)
+    {
+        n = float3(0,1,0);
+        t = float3(0,0,1);
+    }
+    else if (face == 4) // posz (blue)
+    {
+        n = float3(0,0,-1);
+        t = float3(0,1,0);
+    }
+    else // if (i.face == 5) // negz (yellow)
+    {
+        n = float3(0,0,1);
+        t = float3(0,1,0);
+    }
+ 
     float3 x = cross(n, t);
  
     uv = uv * 2 - 1;
@@ -123,34 +150,60 @@ float2 ComputeUvFromNormal(float3 n)
     uv.x = atan2(N.x, N.z) / PI /2 + 1;
     return uv;
 }
-
  
-struct vsOutput
+struct vsOutput 
 {
     float4 position : SV_POSITION;
-    float3 normal : TEXCOORD0;
-    float4 color : COLOR0;
+    float2 uv : TEXCOORD0;
+};
+
+struct gsOutput
+{
     uint faceId : SV_RENDERTARGETARRAYINDEX;
+    float4 position : SV_POSITION;
+    float3 normal : CUSTOM;
+    float4 color : COLOR0;
 };
 
 vsOutput vsMain(uint vertexId: SV_VertexID)
 {
     vsOutput output;
 
-    int faceIndex = vertexId / 6;
+    // uint faceIndex = vertexId / 6;
 
-    float4 quadPos = float4(Quad[vertexId], 1) ;
+    // float4 quadPos = float4(Quad[vertexId], 1) ;
 
-    float2 uv= quadPos.xy*float2(0.5, -0.5) + 0.5;
-    output.normal= UvAndIndexToBoxCoord(uv, faceIndex);
-    output.position = quadPos;
-    output.faceId = faceIndex;
-    output.color = 1;
+    // output.uv= quadPos.xy * float2(0.5, -0.5) + 0.5;
+    //output.position = quadPos;
+    output.uv = float2((vertexId << 1) & 2, vertexId & 2);
+    output.position = float4(output.uv * float2(2,-2) + float2(-1,1), 0, 1);
     return output; 
 }
 
-float4 psMain(in vsOutput i) : SV_TARGET0
+[maxvertexcount(18)]
+void gsMain(triangle vsOutput input[3], inout TriangleStream<gsOutput> output)
 {
-    float2 uv = ComputeUvFromNormal(i.normal) + float2(Orientation, 0);
+    for( int f = 0; f < 6; ++f )
+    {
+        for( int v = 0; v < 3; ++v )
+        {
+            gsOutput o;
+            o.position = input[v].position;
+            o.normal = UvAndIndexToBoxCoord(input[v].uv, f);
+            o.color = colorOfBox(f);// float4(1,1,1,1);
+            o.faceId = f;
+            output.Append(o);
+        }
+        output.RestartStrip();
+    }
+}
+
+
+float4 psMain(in gsOutput i) : SV_TARGET0
+{
+    //return i.color;
+    //return float4( abs(i.normal.rgb),1);
+    //return float4(0,1,0,1);
+    float2 uv = ComputeUvFromNormal(i.normal);//     + float2(Orientation, 0);
     return Image.Sample(texSampler,uv);
 }
